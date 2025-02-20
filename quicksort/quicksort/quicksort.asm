68db253078913755f4e7b0a23fcfb8f9ecfb83db
Kacper Kozak <165217014+kozi00@users.noreply.github.com>
2025-02-20 23:49:01 +0100

    Update quicksort.asm dziala

--- quicksort/quicksort/quicksort.asm
+++ quicksort/quicksort/quicksort.asm
@@ -1,9 +1,9 @@
 .686
 .model flat
 
-_quicksort public
-_partition public
-_swap public
+public _quicksort 
+public _partition 
+public _swap 
 
 
 .code
@@ -11,7 +11,7 @@ _swap public
 _swap PROC
 	push ebp
 	mov ebp, esp
-	push ebx
+	pusha
 	
 	mov eax, [ebp + 8] ;pierwszy element
 	mov edx, [ebp + 12] ;drugi element
@@ -22,7 +22,7 @@ _swap PROC
 	mov [eax], ebx
 	mov [edx], ecx
 
-	pop ebx
+	popa
 	mov esp, ebp
 	pop ebp
 	ret
@@ -35,42 +35,56 @@ _partition PROC
 	push ebx
 	push edi
 	push esi
+	push ecx
+	push eax
 
 	mov eax, [ebp + 8] ;adres tablicy
-	mov ecx, [ebp + 12] ;low
+	;[ebp + 12] ;low
 	mov edx, [ebp + 16] ;high
 
 	mov ebx, [eax + edx * 4] ;pivot
 
-	mov edi, ecx
+	mov edi, [ebp+12] ;low - 1
 	sub edi, 1 ;indeks elementu mniejszego od pivotu do zamiany
 
+	mov ecx, [ebp + 12] ;indeks petli
 petla:
 	cmp [eax + ecx * 4], ebx ;porownanie elementu z pivotem
 	jge dalej ;jezeli element >= pivot to dalej
 
 	inc edi
-	push [eax + edi * 4]
-	push [eax + ecx * 4]
-	call swap;wymiana elementow
+
+	push esi
+	lea esi, [eax + edi * 4]
+	push esi
+	lea esi, [eax + ecx * 4]
+	push esi
+	call _swap;wymiana elementow, dodatkowe push esi i pop esi zeby zapamietac wartosc
 	add esp, 8
+	pop esi
 
 
 dalej:
-	dec edx
-	cmp ecx, edx ;trzeba sprawdzic czy low < high - 1, inc i dec nie zmienia flag
-	inc edx
-	jbe petla
+	mov esi, edx
+	dec esi
+	cmp ecx, esi ;trzeba sprawdzic czy indeks <= high - 1, esi uzyty jako high - 1
+	lea ecx, [ecx + 1] ;zwiekszenie indeksu petli bez zmiany flag
+	jle petla
 
-	push [eax + edi * 4 + 4] ;arr[i+1]
-	push [eax + edx * 4] ;arr[high]
-	call swap ;wymiana elementow
+	push esi
+	lea esi, [eax + edi * 4 + 4] ;arr[i+1]
+	push esi
+	lea esi, [eax + edx * 4] ;arr[high]
+	push esi
+	call _swap ;wymiana elementow, dodatkowe push esi i pop esi zeby zapamietac wartos
 	add esp, 8
+	pop esi
 
 	mov edx, edi
 	add edx, 1 ;zwroc indeks pivotu w edx
 
-
+	pop eax
+	pop ecx
 	pop esi
 	pop edi
 	pop ebx
@@ -88,20 +102,20 @@ _quicksort PROC
 	mov ecx, [ebp + 12] ;low
 	mov ebx, [ebp + 16] ;high
 
-	cmp ecx, edx ;jesli low >= high to koniec
-	jae end
+	cmp ecx, ebx ;jesli low >= high to koniec
+	jge koniec
 
 	push ebx
 	push ecx
 	push eax
-	call partition ;wywolanie funkcji partition
+	call _partition ;wywolanie funkcji partition
 	add esp, 12 ;pivot w edx
 
 	dec edx
 	push edx
 	push ecx
 	push eax
-	call quicksort ;wywolanie funkcji quicksort dla lewej czesci
+	call _quicksort ;wywolanie funkcji quicksort dla lewej czesci
 	add esp, 12
 	inc edx
 
@@ -109,11 +123,11 @@ _quicksort PROC
 	push ebx
 	push edx
 	push eax
-	call quicksort ;wywolanie funkcji quicksort dla prawej czesci
+	call _quicksort ;wywolanie funkcji quicksort dla prawej czesci
 	add esp, 12
 	dec edx
 
-end:
+koniec:
 	pop ebx
 	mov esp, ebp
 	pop ebp
